02a34971bd0da0fb55050177fb9d3a75b8bd16e4
antimonysayz <antimonysayz@gmail.com>
2020-03-14 20:18:40 -0400

    displays auctions index

--- auction_house/app/controllers/auctions_controller.rb
+++ auction_house/app/controllers/auctions_controller.rb
@@ -19,9 +19,23 @@ class AuctionsController < ApplicationController
 
     post '/auctions' do
         @user = current_user
-        @auction = Auction.create(:name => params[:name], :description => params[:description], :current_bid => params[:current_bid])
-        redirect '/auctions'
+        if params[:name].empty? || params[:content].empty? || params[:current_bid].empty?
+            redirect '/auctions/new'
+        end
+            @auction = Auction.create(:name => params[:name], :description => params[:description], :current_bid => params[:current_bid])
+            redirect '/auctions'
     end
+
+    get '/auctions/:id' do
+        if !logged_in?
+            redirect '/login'
+        end
+        @user = current_user
+        @auction = Auction.find(params[:id])
+        erb :'/auctions/show'
+
+    end
+
 
     
 
